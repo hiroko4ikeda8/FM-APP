32a689b856054372a94fd0a37346ab7d1771cacf
hiroko ikeda <art_stylist_spinning@yahoo.co.jp>
2025-02-18 17:26:35 +0900

    Update models, delete LikeFactory, and modify LikesTableSeeder

--- src/app/Models/ShippingAddress.php
+++ src/app/Models/ShippingAddress.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ShippingAddress extends Model
 {
-    use HasFactory;
 
     protected $fillable = ['postal_code', 'building_name'];
 
