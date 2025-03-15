2110fc1563cddd54da5a4c521010330fef2b1b0a
hiroko4ikeda8 <art_stylist_spinning@yahoo.co.jp>
2025-03-16 06:47:27 +0900

    静的プロフィール画面作成

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    //public function showProfile()
+    //{
+    // ユーザー情報を取得（ログイン中のユーザー情報）
+    //$user = auth()->user(); // ログインユーザー情報を取得
+
+
+    //return view('auth.profile', compact('user'));
+    //}
+
+    public function showProfile()
+    {
+        return view('auth.profile'); // ビュー 'resources/views/auth/profile.blade.php' を表示
+    }
 }
--- src/resources/views/auth/profile.blade.php
+++ src/resources/views/auth/profile.blade.php
@@ -3,49 +3,59 @@
 @section('title', 'プロフィール')
 
 @push('styles')
-<link rel="stylesheet" href="{{ asset('css/auth/edit-profile.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth/profile.css') }}">
 @endpush
 
 @section('content')
 <div class="container mt-5">
-    <h2>プロフィール</h2>
-
     <!-- 上のフォーム: アイコンとユーザー名 -->
     <div class="row mb-3">
         <div class="col-md-4">
-            <img src="{{ asset('images/default-avatar.jpg') }}" alt="アイコン" class="img-fluid" id="profile-picture">
+            <img src="{{ asset('storage/images/三毛猫のアイコン.jpg') }}" alt="アイコン" class="img-fluid" id="profile-picture">
         </div>
         <div class="col-md-4">
-            <h4>{{ Auth::user()->username }}</h4> <!-- ユーザー名 -->
+            <h4>テストユーザー</h4> <!-- 仮のユーザー名 -->
         </div>
         <div class="col-md-4">
-            <a href="{{ route('profile.edit') }}" class="btn btn-primary">プロフィールを編集</a> <!-- 編集ボタン -->
+            <a href="#" class="btn btn-primary">プロフィールを編集</a> <!-- 仮の編集ボタン -->
         </div>
     </div>
 
     <!-- 下のフォーム: 商品リスト -->
     <div class="row mt-4">
         <div class="col-md-6">
-            <h5>出品した商品</h5>
-            <ul>
-                @foreach($user->items as $item)
-                <li><a href="{{ route('item.show', $item->id) }}">
-                        <img src="{{ asset('storage/' . $item->image) }}" alt="商品画像" width="50">
-                        {{ $item->name }}
-                    </a></li>
-                @endforeach
+            <p class="custom-line">出品した商品</p>
+            <ul class="list-unstyled">
+                <li>
+                    <a href="#" style="display: block; text-align: center;">
+                        <img src="{{ asset('storage/images/Armani+Mens+Clock.jpg') }}" alt="商品画像" width="150" height="150">
+                    </a>
+                    <a href="#" style="display: block; text-align: center;">サンプル商品1</a>
+                </li>
+                <li>
+                    <a href="#" style="display: block; text-align: center;">
+                        <img src="{{ asset('storage/images/HDD+Hard+Disk.jpg') }}" alt="商品画像" width="150" height="150">
+                    </a>
+                    <a href="#" style="display: block; text-align: center;">サンプル商品2</a>
+                </li>
             </ul>
         </div>
 
         <div class="col-md-6">
-            <h5>購入した商品</h5>
-            <ul>
-                @foreach($user->purchases as $purchase)
-                <li><a href="{{ route('item.show', $purchase->item->id) }}">
-                        <img src="{{ asset('storage/' . $purchase->item->image) }}" alt="商品画像" width="50">
-                        {{ $purchase->item->name }}
-                    </a></li>
-                @endforeach
+            <p class="custom-line">購入した商品</p>
+            <ul class="list-unstyled">
+                <li>
+                    <a href="#" style="display: block; text-align: center;">
+                        <img src="{{ asset('storage/images/Purse+fashion+pocket.jpg') }}" alt="商品画像" width="150" height="150">
+                    </a>
+                    <a href="#" style="display: block; text-align: center;">サンプル商品3</a>
+                </li>
+                <li>
+                    <a href="#" style="display: block; text-align: center;">
+                        <img src="{{ asset('storage/images/外出メイクアップセット.jpg') }}" alt="商品画像" width="150" height="150">
+                    </a>
+                    <a href="#" style="display: block; text-align: center;">サンプル商品4</a>
+                </li>
             </ul>
         </div>
     </div>
