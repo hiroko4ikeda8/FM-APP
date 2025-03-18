d64f546198a6689a6c5e704321c9c34b4b5b559e
hiroko4ikeda8 <art_stylist_spinning@yahoo.co.jp>
2025-03-18 16:08:47 +0900

    商品一覧とプロフィー面の切替画面作成

--- src/resources/views/auth/profile.blade.php
+++ src/resources/views/auth/profile.blade.php
@@ -4,59 +4,107 @@
 
 @push('styles')
 <link rel="stylesheet" href="{{ asset('css/auth/profile.css') }}">
+<link rel="stylesheet" href="{{ asset('css/header-basic.css') }}">
 @endpush
 
 @section('content')
 <div class="container mt-5">
     <!-- 上のフォーム: アイコンとユーザー名 -->
     <div class="row mb-3">
-        <div class="col-md-4">
+        <div class="col-md-3">
             <img src="{{ asset('storage/images/三毛猫のアイコン.jpg') }}" alt="アイコン" class="img-fluid" id="profile-picture">
         </div>
-        <div class="col-md-4">
-            <h4>テストユーザー</h4> <!-- 仮のユーザー名 -->
+        <div class="col-md-3">
+            <h3>テストユーザー</h3> <!-- 仮のユーザー名 -->
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <a href="/mypage/profile" class="btn btn-primary">プロフィールを編集</a> <!-- 仮の編集ボタン -->
         </div>
     </div>
-
-    <!-- 下のフォーム: 商品リスト -->
-    <div class="row mt-4">
-        <div class="col-md-6">
-            <p class="custom-line">出品した商品</p>
-            <ul class="list-unstyled">
-                <li>
-                    <a href="#" style="display: block; text-align: center;">
-                        <img src="{{ asset('storage/images/Armani+Mens+Clock.jpg') }}" alt="商品画像" width="150" height="150">
-                    </a>
-                    <a href="#" style="display: block; text-align: center;">サンプル商品1</a>
-                </li>
-                <li>
-                    <a href="#" style="display: block; text-align: center;">
-                        <img src="{{ asset('storage/images/HDD+Hard+Disk.jpg') }}" alt="商品画像" width="150" height="150">
-                    </a>
-                    <a href="#" style="display: block; text-align: center;">サンプル商品2</a>
-                </li>
-            </ul>
+</div>
+<div class="main-content">
+    <nav class="nav-container border-bottom pb-2 mb-3">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active" id="buyTab" data-bs-toggle="pill" href="#buy">購入した商品</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="sellTab" data-bs-toggle="pill" href="#sell">出品した商品</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="tab-content mt-3">
+        <div id="buy" class="tab-pane fade show active">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Armani+Mens+Clock.jpg') }}" alt="商品1画像" class="img-fluid fixed-size">
+                            <p>腕時計</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Tumbler+souvenir.jpg') }}" alt="商品2画像" class="img-fluid fixed-size">
+                            <p>タンブラー</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Music+Mic+4632231.jpg') }}" alt="商品3画像" class="img-fluid fixed-size">
+                            <p>マイク</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Purse+fashion+pocket.jpg') }}" alt="商品3画像" class="img-fluid fixed-size">
+                            <p>ショルダーバッグ</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class="col-md-6">
-            <p class="custom-line">購入した商品</p>
-            <ul class="list-unstyled">
-                <li>
-                    <a href="#" style="display: block; text-align: center;">
-                        <img src="{{ asset('storage/images/Purse+fashion+pocket.jpg') }}" alt="商品画像" width="150" height="150">
-                    </a>
-                    <a href="#" style="display: block; text-align: center;">サンプル商品3</a>
-                </li>
-                <li>
-                    <a href="#" style="display: block; text-align: center;">
-                        <img src="{{ asset('storage/images/外出メイクアップセット.jpg') }}" alt="商品画像" width="150" height="150">
-                    </a>
-                    <a href="#" style="display: block; text-align: center;">サンプル商品4</a>
-                </li>
-            </ul>
+        <div id="sell" class="tab-pane fade">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Armani+Mens+Clock.jpg') }}" alt="出品商品1" class="img-fluid fixed-size">
+                            <p>腕時計</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/iLoveIMG+d.jpg') }}" alt="出品商品2" class="img-fluid fixed-size">
+                            <p>玉ねぎ</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Waitress+with+Coffee+Grinder.jpg') }}" alt="出品商品3" class="img-fluid fixed-size">
+                            <p>コーヒーミル</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Living+Room+Laptop.jpg') }}" alt="出品商品3" class="img-fluid fixed-size">
+                            <p>ノートパソコン</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -10,8 +10,6 @@
 @section('content')
 <div class="main-content">
     <!-- 商品一覧のコンテンツ -->
-    <h1>商品一覧</h1>
-
     <!-- ナビゲーションメニュー -->
     <nav class="nav-container border-bottom pb-2 mb-3">
         <ul class="nav nav-pills">
@@ -23,59 +21,81 @@
             </li>
         </ul>
     </nav>
-
-    <p>ここに商品一覧を表示</p>
     <!-- 商品表示 -->
     <div class="tab-content mt-3">
         <!-- おすすめ商品 -->
         <div id="recommend" class="tab-pane fade show active">
-            <div class="items">
-                <!-- 静的なダミーデータを表示 -->
-                <div class="item">
-                    <a href="#">
-                        <img src="{{ asset('storage/images/Armani+Mens+Clock.jpg') }}" alt="商品1画像" class="img-fluid fixed-size">
-                        <p>腕時計</p>
-                    </a>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Armani+Mens+Clock.jpg') }}" alt="商品1画像" class="img-fluid fixed-size">
+                            <p>腕時計</p>
+                        </a>
+                    </div>
                 </div>
-                <div class="item">
-                    <a href="#">
-                        <img src="{{ asset('storage/images/Tumbler+souvenir.jpg') }}" alt="商品2画像" class="img-fluid fixed-size">
-                        <p>タンブラー</p>
-                    </a>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Tumbler+souvenir.jpg') }}" alt="商品2画像" class="img-fluid fixed-size">
+                            <p>タンブラー</p>
+                        </a>
+                    </div>
                 </div>
-                <div class="item">
-                    <a href="#">
-                        <img src="{{ asset('storage/images/Music+Mic+4632231.jpg') }}" alt="商品3画像" class="img-fluid fixed-size">
-                        <p>マイク</p>
-                    </a>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Music+Mic+4632231.jpg') }}" alt="商品3画像" class="img-fluid fixed-size">
+                            <p>マイク</p>
+                        </a>
+                    </div>
                 </div>
-                <!-- 他の商品を追加 -->
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Purse+fashion+pocket.jpg') }}" alt="商品4画像" class="img-fluid fixed-size">
+                            <p>ショルダーバッグ</p>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
 
         <!-- マイリスト商品 -->
         <div id="mylist" class="tab-pane fade">
-            <div class="items">
-                <!-- マイリストの商品をここに追加 -->
-                <div class="item">
-                    <a href="#">
-                        <img src="{{ asset('storage/images/Armani+Mens+Clock.jpg') }}" alt="マイリスト商品1" class="img-fluid fixed-size">
-                        <p>腕時計</p>
-                    </a>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Armani+Mens+Clock.jpg') }}" alt="マイリスト商品1" class="img-fluid fixed-size">
+                            <p>腕時計</p>
+                        </a>
+                    </div>
                 </div>
-                <div class="item">
-                    <a href="#">
-                        <img src="{{ asset('storage/images/iLoveIMG+d.jpg') }}" alt="マイリスト商品2" class="img-fluid fixed-size">
-                        <p>玉ねぎ</p>
-                    </a>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/iLoveIMG+d.jpg') }}" alt="マイリスト商品2" class="img-fluid fixed-size">
+                            <p>玉ねぎ</p>
+                        </a>
+                    </div>
                 </div>
-                <div class="item">
-                    <a href="#">
-                        <img src="{{ asset('storage/images/Waitress+with+Coffee+Grinder.jpg') }}" alt="マイリスト商品3" class="img-fluid  fixed-size">
-                        <p>コーヒーミル</p>
-                    </a>
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Waitress+with+Coffee+Grinder.jpg') }}" alt="マイリスト商品3" class="img-fluid fixed-size">
+                            <p>コーヒーミル</p>
+                        </a>
+                    </div>
                 </div>
-                <!-- 他のマイリスト商品も追加可能 -->
+                <div class="col-md-3">
+                    <div class="item text-center">
+                        <a href="#">
+                            <img src="{{ asset('storage/images/Living+Room+Laptop.jpg') }}" alt="マイリスト商品4" class="img-fluid fixed-size">
+                            <p>ノートパソコン</p>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
